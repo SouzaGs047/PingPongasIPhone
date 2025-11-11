cc207b82c156635478b6697da119d6ba3aeb8903
Gugas <souza.gs047@gmail.com>
2025-11-11 14:52:37 -0300

    Initial Commit

---
+++ PingPongasIPhone/PingPongasIPhoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  PingPongasIPhoneApp.swift
+//  PingPongasIPhone
+//
+//  Created by Gustavo Souza Santana on 11/11/25.
+//
+
+import SwiftUI
+
+@main
+struct PingPongasIPhoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
